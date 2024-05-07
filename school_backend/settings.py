fa42f9c1a543bc94db3f8faccdb2a5b0d640ebf9
Anwasi Ayebaemi <ayewealth11@gmail.com>
2024-05-07 10:48:45 +0100

    CorsHeader

--- school_backend/settings.py
+++ school_backend/settings.py
@@ -183,6 +183,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
+    "https://bdoms.vercel.app"
 ]
 
 SIMPLE_JWT = {
